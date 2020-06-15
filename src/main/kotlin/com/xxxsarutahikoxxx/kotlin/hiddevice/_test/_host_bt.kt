677b84c20048215b8d19a8e584ac77ee15dfab96
tshom_000 <tshomeaddress@gmail.com>
2020-06-15 15:11:45 +0900

    ver 1.0.10

--- src/main/kotlin/com/xxxsarutahikoxxx/kotlin/hiddevice/_test/_host_bt.kt
+++ src/main/kotlin/com/xxxsarutahikoxxx/kotlin/hiddevice/_test/_host_bt.kt
@@ -7,6 +7,7 @@ import com.xxxsarutahikoxxx.kotlin.SocketRunner.SocketRunner
 import com.xxxsarutahikoxxx.kotlin.hiddevice.core.HIDData
 import com.xxxsarutahikoxxx.kotlin.hiddevice.core.HIDDeviceJIS
 import com.xxxsarutahikoxxx.kotlin.hiddevice.core.HIDExporter
+import com.xxxsarutahikoxxx.kotlin.hiddevice.runner.BTClientRunner
 import com.xxxsarutahikoxxx.kotlin.hiddevice.utilitys.JISHost
 import com.xxxsarutahikoxxx.kotlin.hiddevice.utilitys.out
 import java.awt.Robot
@@ -15,8 +16,11 @@ import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
     TrayFrame("BT-Host", false).apply {
-        val runner = BTHostRunner()
-        runner.open()
+//        val runner = BTHostRunner()
+//        runner.open()
+
+        val runner = BTClientRunner(BluetoothRunner.device { it.getFriendlyName(true) == "d-01J" })
+        runner.connect()
 
         // これがないと読み込みのタイミング制御がうまくいかない
 //        thread {
@@ -25,6 +29,8 @@ fun main(args: Array<String>) {
 //                runner.writeObject(System.currentTimeMillis())
 //            }
 //        }
+
+
     }
 
 }
--- src/main/kotlin/com/xxxsarutahikoxxx/kotlin/hiddevice/runner/Runner.kt
+++ src/main/kotlin/com/xxxsarutahikoxxx/kotlin/hiddevice/runner/Runner.kt
@@ -1,9 +1,6 @@
 package com.xxxsarutahikoxxx.kotlin.hiddevice.runner
 
-import com.xxxsarutahikoxxx.kotlin.SocketRunner.BluetoothRunner
-import com.xxxsarutahikoxxx.kotlin.SocketRunner.ClientWebRunner
-import com.xxxsarutahikoxxx.kotlin.SocketRunner.HostBluetoothRunner
-import com.xxxsarutahikoxxx.kotlin.SocketRunner.HostWebRunner
+import com.xxxsarutahikoxxx.kotlin.SocketRunner.*
 import com.xxxsarutahikoxxx.kotlin.Utilitys.outstream
 import com.xxxsarutahikoxxx.kotlin.hiddevice.core.HIDData
 import com.xxxsarutahikoxxx.kotlin.hiddevice.core.HIDExporter
@@ -12,6 +9,7 @@ import com.xxxsarutahikoxxx.kotlin.hiddevice.utilitys.out
 import java.io.Serializable
 import java.lang.RuntimeException
 import java.util.*
+import javax.bluetooth.RemoteDevice
 
 
 open class HostRunner(port : Int) : HostWebRunner(port, true){
@@ -32,6 +30,24 @@ open class ClientRunner(address : String = "localhost", port : Int) : ClientWebR
 open class BTHostRunner : HostBluetoothRunner {
     constructor() : super()
     constructor(uuid : String) : super(uuid)
+
+    var time = System.currentTimeMillis() to System.currentTimeMillis()
+    override fun onAccept(obj: Serializable) {
+        when( obj ){
+            is HIDData -> { obj.invoke() }
+            is String -> { outstream("$obj") }
+            is Long -> {
+                val (send, accept) = obj to System.currentTimeMillis()
+
+                out = "Send : ${send - time.first}, Accept : ${accept - time.second}"
+                time = send to accept
+            }
+        }
+    }
+}
+open class BTClientRunner : ClientBluetoothRunner {
+    constructor(device : RemoteDevice) : super(device)
+    constructor(device : RemoteDevice, uuid : String) : super(device, uuid)
 
     var time = System.currentTimeMillis() to System.currentTimeMillis()
     override fun onAccept(obj: Serializable) {
